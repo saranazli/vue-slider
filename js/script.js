deb80ddfd2ed705453fb25f4297681e6a1a723d1
s4r44d43 <sararez@live.it>
2024-03-12 13:40:56 +0100

    [set] layout

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 
-const carouselImgs = [
-  './img/01.webp',
-  './img/02.webp',
-  './img/03.webp',
-  './img/04.webp',
-  './img/05.webp'
-];
+const { createApp } = Vue;
+
+createApp({
+
+  data(){
+
+    return{
+
+      img : [
+
+        './img/01.webp',
+        './img/02.webp',
+        './img/03.webp',
+        './img/04.webp',
+        './img/05.webp'
+
+      ]
+
+    }
+  },
+
+  methods:{
+
+
+
+  },
+
+  mounted(){
+
+
+    
+  }
+
+}).mount('#app');
