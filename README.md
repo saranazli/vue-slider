9c74cd96e6756dc34829d5d6b8b14dfabea436e9
s4r44d43 <sararez@live.it>
2024-03-12 13:31:16 +0100

    [start]

---
+++ README.md
@@ -0,0 +1,6 @@
+Title
+===
+Abstract:xxx
+## Svolgimento
+
+1. 
---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const carouselImgs = [
+  './img/01.webp',
+  './img/02.webp',
+  './img/03.webp',
+  './img/04.webp',
+  './img/05.webp'
+];
